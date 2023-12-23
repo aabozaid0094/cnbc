2f66c25d4eabe075742f372ec9845de51625febf
Ahmed Abo Zaid <aabozaid0094@gmail.com>
2023-12-23 09:30:23 +0200

    Add trapezoid area algorithm

--- Algorithms/area/index.js
+++ Algorithms/area/index.js
@@ -5,4 +5,11 @@ Processes:-
 Output: area
 */
 exports.parallelogramArea = (base, height) => base * height;
+/*
+Inputs: base1, base2, height
+Processes:-
+- Get average base by summing the two parallel bases and divide by 2
+- Multiply average base by height to get area
+Output: area
+*/
 exports.trapezoidArea = (base1, base2, height) => ((base1+base2)/2) * height;

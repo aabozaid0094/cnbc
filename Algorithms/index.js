16eb8db081d944e4cd3046386a4339b8a62a3b4b
Ahmed Abo Zaid <aabozaid0094@gmail.com>
2023-12-23 10:38:03 +0200

    Add more details to area console notation

--- Algorithms/index.js
+++ Algorithms/index.js
@@ -1,4 +1,4 @@
 const area = require('./area');
 
-console.log("Parallelogram Area:", area.parallelogramArea(10, 5));
-console.log("Trapezoid Area:", area.trapezoidArea(10, 8, 5));
+console.log("Parallelogram Area of base 10 and height 5:", area.parallelogramArea(10, 5));
+console.log("Trapezoid Area of base1 10 and base2 8 and height 5:", area.trapezoidArea(10, 8, 5));

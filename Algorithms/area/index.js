28bf8e7603eddc8a6cc9a83aad569999417233c2
Ahmed Abo Zaid <aabozaid0094@gmail.com>
2023-12-23 09:28:01 +0200

    Add parallelogram area algorithm

--- Algorithms/area/index.js
+++ Algorithms/area/index.js
@@ -1,2 +1,8 @@
+/*
+Inputs: base, height
+Processes:-
+- Multiply base by height to get area
+Output: area
+*/
 exports.parallelogramArea = (base, height) => base * height;
 exports.trapezoidArea = (base1, base2, height) => ((base1+base2)/2) * height;
